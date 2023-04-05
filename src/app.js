348c132764cbb536985f97997bf813b851263b0d
KayBahr <kaybear0002@gmail.com>
2023-04-05 22:50:17 +0000

    domain generate commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,13 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let generateExcuse = function generateExcuse() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+};
+
+// Sender window in parent domain
+var iframe = document.getElementById("myIframe"); // Replace 'myIframe' with the ID of your iframe
+var message = "Hello from parent domain!";
