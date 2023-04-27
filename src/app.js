e0ce33d08fc07dfbb325ed8faa86adb5e947d2f4
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-04-27 22:01:27 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateDomain();
 };
 
-let generateExcuse = function generateExcuse() {
+function generateDomain() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-};
 
-// Sender window in parent domain
-var iframe = document.getElementById("myIframe"); // Replace 'myIframe' with the ID of your iframe
-var message = "Hello from parent domain!";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + ".com";
+        console.log(domain);
+      }
+    }
+  }
+}
